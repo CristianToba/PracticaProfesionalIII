9f5caa220e8e396598c7e14ea99201dd6d7eef75
CristianTobares <CristianTobares@Solitario>
2017-04-02 19:04:51 -0300

    Merge origin/master
    Conflicts:
    	index.html

--- js/ListarAgendaPorProfesional.php
+++ js/ListarAgendaPorProfesional.php
@@ -2,39 +2,52 @@
 
 require_once('../Conexion/Conexion.php');
 $valorIdProf=$_GET['idProf'];
-$sql = "SELECT * from turno where idMedico=$valorIdProf";
+$sqlAgenda = "SELECT T.idTurno, T.fechaTurno, T.nroTurno, C.nombreconsultorio, CL.nombre,CL.direccion, C.idMedico, H.HorarioInicio,H.HorarioFin,T.baja,T.Habilitado, P.nombre, P.apellido,E.especialidad,PE.nombre,PE.apellido
+FROM turno AS T INNER JOIN CONSULTORIO AS C ON
+T.idconsultorio=C.idConsultorio INNER JOIN Clinica AS CL ON
+C.idClinica=CL.cuil INNER JOIN Persona AS P ON
+C.idMedico=P.nroPersona AND P.tipoPers=2 INNER JOIN Especialidad AS E ON
+P.idEspecialidad=E.idEspecialidad
+INNER JOIN Horarios AS H ON 
+T.idHorario= H.idhorarios
+INNER JOIN Persona AS PE ON
+T.idPersona=PE.nroPersona
+ WHERE T.idMedico=$valorIdProf";
 echo ($sql);
 $serverName = "(local)";
 $connectionInfo = array("Database" => "DAMSU", "UID" => "DAMSU", "PWD" => "DAMSU");
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-$stmt = sqlsrv_query($conn, $sql);
-$persona = array(); //creamos un array
-
+$stmt = sqlsrv_query($conn, $sqlAgenda);
+$agendaMedico = array(); //creamos un array
+                                                                           
 while ($row = sqlsrv_fetch_array($stmt)) {
     
-    $id = $row['nroPersona'];
-    $Dni = $row['dni'];
-    $Nombre = $row['nombre'];
-    $Apellido = $row['apellido'];
-    $Email = $row['mail'];
-    $FNac = $row['fechaNac']; 
-    $Nacionalidad = $row['nacionalidad'];
-    $ECivil = $row['estadoCivil'];
-    
-    $telurgencia = $row['telUrgencia'];
-    $celular = $row['celular'];
-    $oSocial = $row['obraSocial'];
-    $habilitado = $row['Habilitado'];
+    $idTurno = $row['idTurno'];
+    $fechaTurno = $row['fechaTurno'];
+    $nroturno = $row['nroTurno'];
+    $consultorio = $row['nombreconsultorio'];
+    $clinicaNombre = $row['nombreClinica']; 
+    $clinicaDir = $row['direccionClinica']; 
+    $matriculaMed= $row['idMedico'];
+    $horarioI= $row['HorarioInicio'];
+    $horarioF = $row['HorarioFinal'];
+    $bajaTurno = $row['baja'];    
+    $estadoTurno = $row['Habilitado'];
+    $nombreMed = $row['nombreMedico'];
+    $apellidoMed = $row['apellidoMedico'];
+    $especialidadMed = $row['especialidad'];
+    $nombrePers = $row['nombrePersona'];
+    $apellidoPers = $row['apellidoPersona'];
 
-    $persona[] = array('id' => $id, 'dni' => $Dni,'nombre' => $Nombre, 'apellido' => $Apellido,'mail' => $Email, 'fechaNac' => $FNac,
-        'nacionalidad' => $Nacionalidad,'EstCivil' => $ECivil,'telUrgencia' => $telurgencia,
-        'celular' => $celular,'obraSocial' => $oSocial,'Habilitado' => $habilitado);
+    $agendaMedico[] = array('idTurno' => $idTurno, 'fechaTurno' => $fechaTurno,'nroTurno' => $nroturno, 'consultorio' => $consultorio,'clinicaNombre' => $clinicaNombre,'clinicaDir' => $clinicaDir , 
+        'matriculaMed' => $matriculaMed, 'horarioI' => $horarioI, 'horarioF' => $horarioF, 'bajaTurno' => $bajaTurno, 'estadoTurno' => $estadoTurno,  'nombreMed' => $nombreMed , 'apellidoMed' => $apellidoMed,
+        'especialidadMed' => $especialidadMed, 'nombrePers' => $nombrePers, 'apeliidoPers' => $apellidoPers);
         
 }
 
 //Creamos el JSON
 
-echo json_encode($persona,true);
+echo json_encode($agendaMedico,true);
 
 //desconectamos la base de datos
 sqlsrv_close($conn) or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
