c3efbbd8d5a990750f1a280b437cec7dc74b64b4
CristianToba <tobares.cristian@gmail.com>
2017-11-21 14:20:52 -0300

    Create Session.php

---
+++ Funciones/Session.php
@@ -0,0 +1,5 @@
+http://www.tutorialesprogramacionya.com/phpya/temarios/descripcion.php?cod=58&punto=&inicio=
+http://www.tutores.org/codigo/361/guardar-y-leer-variables-de-sesion/
+https://wlannot.wordpress.com/2007/03/05/validacion-o-autenticacion-de-usuarios-en-php-segunda-parte/
+https://www.codejobs.biz/es/blog/2013/01/29/crear-una-sesion-en-php-session
+http://velozityweb.com/blog/php/login-de-usuarios-y-creacion-de-sesiones-con-php-y-mysql/#sthash.jaK57MG2.dpbs
